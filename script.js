6a663135d76e905aafdb7f231a4c93bb89d44f4f
kerekesjuca <kerekes.juca@gmail.com>
2024-02-20 14:13:21 +0100

    alert hozzáadása

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ function myFunction() {
   ul = document.getElementById("plantsearch");
   li = ul.getElementsByTagName('li');
 
-   for (i = 0; i < li.length; i++) {
+  var found = false;
+
+  for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       li[i].style.display = "";
+      found = true;
     } else {
       li[i].style.display = "none";
+
     }
+  }
+  if (!found) {
+    alert("Oldalunkon nem található a keresett növény.");
   }
 }
